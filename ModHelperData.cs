be09fae17ec9b49d0ffb1bffd3958520b7a13b3c
GluPixel <snowballelf1907@gmail.com>
2026-04-28 18:48:18 +1200

    Update version number to 1.0.0

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace ClickToPop;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "52.0";
-    public const string Version = "2.1.5";
+    public const string Version = "1.0.0";
     public const string Name = "Click To Pop";
 
     public const string Description = "Click bloons to pop them!";
